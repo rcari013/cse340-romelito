1a3432e189f439bcf4155cbd20cbba3bae87effc
rcari013 <carinorome@gmail.com>
2025-10-27 16:41:05 +0800

    home page created

--- server.js
+++ server.js
@@ -6,23 +6,28 @@ import express from "express";
 import expressLayouts from "express-ejs-layouts";
 import dotenv from "dotenv";
 import { Client } from "pg";
+import path from "path";
+import { fileURLToPath } from "url";
 import staticRoutes from "./routes/static.js";
 
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
-// Middleware
-app.use(staticRoutes);
+// Serve static files
+app.use(express.static(path.join(__dirname, "public")));
 
 // View Engine and Layouts
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(expressLayouts);
 app.set("layout", "./layouts/layout");
 
-// Server Information
-const port = process.env.PORT || 5500;
-const host = process.env.HOST || "0.0.0.0";
+// Middleware
+app.use(staticRoutes);
 
 // Index Route
 app.get("/", (req, res) => {
@@ -42,7 +47,10 @@ app.get("/db-check", async (req, res) => {
   }
 });
 
-// Start Server
+// Server Info
+const port = process.env.PORT || 5500;
+const host = process.env.HOST || "0.0.0.0";
+
 app.listen(port, host, () => {
   console.log(`✅ Server running at http://${host}:${port}`);
 });
